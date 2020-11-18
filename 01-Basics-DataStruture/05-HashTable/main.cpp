07a7dc646f2bde9ca8ddccaada67fd734f3275d0
currynihao <curryhuangnihao@163.com>
2020-11-18 13:47:50 +0800

    change the function code

--- 01-Basics-DataStruture/05-HashTable/main.cpp
+++ 01-Basics-DataStruture/05-HashTable/main.cpp
@@ -3,6 +3,72 @@
 
 using namespace std;
 
+//散列表的定义
+template<typename DataType> class HashTable
+{
+public:
+    HashTable(int size)
+    {
+        maxSize = size;
+        count = 0;
+        elements = new DataType[size];
+        if(elements == NULL)
+        {
+            exit(1);
+        }
+        for(int i=0 ; i<size ; i++)
+        {
+            elements[i] = NULL;
+        }
+    }
+    ~HashTable()
+    {
+        delete[] elements;
+    }
+
+    //散列函数
+    int hash(DataType value);
+
+    //查找函数
+    int searchHash(DataType value);
+
+    //获取散列表的第i个元素
+    DataType getData(int i)
+    {
+        if(i<=o)
+        {
+            cout << "索引值错误，必须为正整数" << endl;
+        }
+        return elements[i-1];
+    }
+
+    //插入操作
+
+
+private:
+    int maxSize;
+    int count;
+    DataType *elements;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 int main(int argc, char *argv[])
 {
