d934c9564bd58b1e4d6abb060c63ea3dbff237cb
currynihao <curryhuangnihao@163.com>
2020-12-16 10:44:44 +0800

    changedode

--- 02-Tree-Structure/01-BinaryTree/main.cpp
+++ 02-Tree-Structure/01-BinaryTree/main.cpp
@@ -1,6 +1,60 @@
 ﻿#include <QCoreApplication>
 #include <iostream>
 using namespace std;
+
+
+template<typename DataType>
+class BinaryTreeNode
+{
+public:
+    BinaryTreeNode()
+    {
+        data = NULL;
+        lchild = NULL;
+        rchild = NULL;
+    }
+    BinaryTreeNode(DataType newData)
+    {
+        data = newData;
+        lchild = rchild = NULL;
+    }
+
+private:
+    DataType data;
+    BinaryTreeNode *lchild;
+    BinaryTreeNode *rchild;
+};
+
+template<typename DataType>
+class BinaryTree
+{
+public:
+    BinaryTree()
+    {
+        root = new BinaryTreeNode<DataType>();
+    }
+
+    void preOrder();
+
+    void inOrder();
+
+    void postOrder();
+
+    void layOrder();
+
+
+
+private:
+    BinaryTreeNode<DataType> *root;
+};
+
+template<typename DataType>
+void
+
+
+
+
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
