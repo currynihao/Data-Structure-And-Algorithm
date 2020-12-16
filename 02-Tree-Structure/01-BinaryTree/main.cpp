165c0b52609687235c9a133042bad6b4794ef1d4
currynihao <curryhuangnihao@163.com>
2020-12-16 10:22:15 +0800

    cangecode

--- 02-Tree-Structure/01-BinaryTree/main.cpp
+++ 02-Tree-Structure/01-BinaryTree/main.cpp
@@ -1,8 +1,10 @@
-#include <QCoreApplication>
-
+﻿#include <QCoreApplication>
+#include <iostream>
+using namespace std;
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
+    cout << "ddd" << endl;
     return a.exec();
 }
